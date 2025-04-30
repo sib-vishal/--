c892875e1eefbce2328423a7d98e39c06a513eba
sib-vishal <vishal.maurya@sibinfo.tech>
2025-04-30 11:40:59 +0530

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -71,9 +71,8 @@ const Hero = () => {
               <Image
                 width={700}
                 height={500}
-              
+                priority={true}
                 className="w-full"
-                
                 src="https://cdn.rareblocks.xyz/collection/celebration/images/hero/2/hero-img.png"
                 alt=""
               />
