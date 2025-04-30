af79f9b62fb9ab320f077dc36d295f81aebc4b2d
sib-vishal <vishal.maurya@sibinfo.tech>
2025-04-30 12:36:33 +0530

    commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,14 @@
 import dynamic from "next/dynamic";
 import React from "react";
 
-const Hero = dynamic(() => import("@/components/Hero"));
-const About = dynamic(() => import("@/components/About"),{ ssr: false });
-const Services = dynamic(() => import("@/components/Services"),{ ssr: false });
-const Cta_1 = dynamic(() => import("@/components/Cta_1"),{ ssr: false });
-const Projects = dynamic(() => import("@/components/Projects"),{ ssr: false });
+const Hero = dynamic(() => import("@/components/Hero"),{ ssr: true });
+const About = dynamic(() => import("@/components/About"));
+const Services = dynamic(() => import("@/components/Services"));
+const Cta_1 = dynamic(() => import("@/components/Cta_1"));
+const Projects = dynamic(() => import("@/components/Projects"));
 const Blog = dynamic(() => import("@/components/Blog"),{ ssr: false });
-const Faq = dynamic(() => import("@/components/Faq"),{ ssr: false });
-const Contact = dynamic(() => import("@/components/Contact"),{ ssr: false });
+const Faq = dynamic(() => import("@/components/Faq"));
+const Contact = dynamic(() => import("@/components/Contact"));
 
 const Home = () => {
   return (
